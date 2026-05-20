ec45f858f211bfb6f08b9f6b2eea9061325df7a8
koushalya <koushalyamagatapalli@gmail.com>
2026-05-20 18:25:04 +0530

    Add script to categorize mixed list items

---
+++ task1.py
@@ -0,0 +1,17 @@
+mixed_list = [10, "Python", 3.14, 25, "Code", 7.8, "Internship"]
+
+integers = []
+strings = []
+floats = []
+
+for item in mixed_list:
+    if type(item) == int:
+        integers.append(item)
+    elif type(item) == str:
+        strings.append(item)
+    elif type(item) == float:
+        floats.append(item)
+
+print("Integers:", integers)
+print("Strings:", strings)
+print("Floats:", floats)
