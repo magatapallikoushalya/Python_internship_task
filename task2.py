1ab87f25359b8fee67e1077a9cda4c2991f47f5e
koushalya <koushalyamagatapalli@gmail.com>
2026-05-20 19:30:38 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,20 @@
+
+balance = 1000
+
+print("Initial Balance:", balance)
+
+
+deposit = 500
+balance += deposit
+print("After Deposit:", balance)
+
+
+withdraw = 300
+balance -= withdraw
+print("After Withdrawal:", balance)
+
+balance *= 2
+print("After Interest:", balance)
+
+balance /= 2
+print("Final Balance:", balance)
