9b0026259ca7c574fd5f35041861b14f552bae3d
koushalya <koushalyamagatapalli@gmail.com>
2026-05-21 18:24:26 +0530

    Delete task5.py

--- task5.py
+++
@@ -1,8 +0,0 @@
-text = "PythonInternship"
-
-print("Original Text:", text)
-
-print("First 6 letters:", text[:6])
-print("Last 10 letters:", text[-10:])
-print("Alternate letters:", text[::2])
-print("Reversed Text:", text[::-1])
