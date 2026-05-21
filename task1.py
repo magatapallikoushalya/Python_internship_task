d88ee5ab64b4e86ff727e16f96cb32f958f5422d
koushalya <koushalyamagatapalli@gmail.com>
2026-05-21 18:28:09 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,9 +1,7 @@
 mixed_list = [10, "Python", 3.14, 25, "Code", 7.8, "Internship"]
-
 integers = []
 strings = []
 floats = []
-
 for item in mixed_list:
     if type(item) == int:
         integers.append(item)
@@ -11,7 +9,6 @@ for item in mixed_list:
         strings.append(item)
     elif type(item) == float:
         floats.append(item)
-
 print("Integers:", integers)
 print("Strings:", strings)
 print("Floats:", floats)
