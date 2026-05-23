db0dae368af588e2417686fd385e7b0431808fde
koushalya <koushalyamagatapalli@gmail.com>
2026-05-23 17:49:51 +0530

    Add text formatter script in task5.py

---
+++ task5.py
@@ -0,0 +1,18 @@
+
+text = "Python Internship Program"
+print("\n===== TEXT FORMATTER =====\n")
+print("Original Text        :", text)
+
+print("First 6 Characters   :", text[:6])
+
+print("Last 7 Characters    :", text[-7:])
+
+print("Alternate Characters :", text[::2])
+
+print("Reversed Text        :", text[::-1])
+
+print("Uppercase Text       :", text.upper())
+
+print("Lowercase Text       :", text.lower())
+
+print("\n===== FORMAT COMPLETED =====")
